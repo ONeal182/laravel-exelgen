26a80ca8914abdcd69717263f2ff274404adbd7f
Vladimir <vladimir@rupor.agency>
2021-03-18 11:56:43 +0300

    add PDF pack

---
+++ resources/views/web/layout/header.blade.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AOSR</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Oswald:wght@500&display=swap" rel="stylesheet">
+    <!-- Bootstrap --> 
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset("assets/css/style.css") }}">
+</head>
